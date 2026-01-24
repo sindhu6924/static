0dd8a4653b6d31b35f3fd0c28ca81754a6c9c8f2
sindhu6924 <kundurisindhumanjusha2004@gmail.com>
2026-01-24 21:54:10 +0530

    initial

---
+++ src/main/java/com/example/crud/controller.java
@@ -0,0 +1,13 @@
+package com.example.crud;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+	@GetMapping("/display")
+	public String get() {
+		return "hi guys";
+	}
+
+}
