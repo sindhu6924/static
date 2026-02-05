71274775ecbb952480973528a6ccf7ae9187bc41
sindhu6924 <kundurisindhumanjusha2004@gmail.com>
2026-02-05 15:56:17 +0530

    last

--- src/main/java/com/example/crud/controller.java
+++ src/main/java/com/example/crud/controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class controller {
 	@GetMapping("/display")
 	public String get() {
-		return "hi sindhu | you can do it";
+		return "hi sindhu | you can do it |last try";
 	}
 
 }
